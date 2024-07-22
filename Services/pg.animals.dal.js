eb008145126ddee51a8525261a5bbabf25dc37fc
lpnl3456 <luke.peddle@keyin.com>
2024-07-22 10:41:34 -0230

    Added a page to display the 23503 error

--- Services/pg.animals.dal.js
+++ Services/pg.animals.dal.js
@@ -1,14 +1,16 @@
 const dal = require("./zoo_db");
 const DEBUG = true;
-//get all actors.
+//get all animals.
 var getAnimals = function() {
   if(DEBUG) console.log("animals.pg.dal.getAnimals()");
   return new Promise(function(resolve, reject) {
+    //The select statement to get all the animals 
     const sql = "SELECT * FROM public.\"Animals\"\
 ORDER BY \"Animal_ID\" ASC;"
+    //query the data using  sql
     dal.query(sql, [], (err, result) => {
       if (err) {
-        // logging should go here
+      
         if(DEBUG) console.log(err);
         reject(err);
       } else {
@@ -18,11 +20,15 @@ ORDER BY \"Animal_ID\" ASC;"
   }); 
 };
 
+//Add an animal
 var addAnimal = function(name, age, spName) {
   if(DEBUG) console.log("species.pg.dal.addSpecies()");
   return new Promise(function(resolve, reject) {
+    //Insert statement to add the animal
     const sql = "INSERT INTO public.\"Animals\"(\"Name\", \"Age\",\"Species_Name\") \
         VALUES ($1, $2, $3)";
+
+        //query the data using the sql and the data imported from the form
     dal.query(sql, [name, age, spName], (err, result) => {
       if (err) {
           if(DEBUG) console.log(err);
@@ -34,26 +40,14 @@ var addAnimal = function(name, age, spName) {
   });
 };
 
-var getAnimalById = function(id) {
-  if(DEBUG) console.log("species.pg.dal.getgetSpecieBySpecieName()");
-  return new Promise(function(resolve, reject) {
-    const sql = "SELECT \"Aniaml_ID\" AS _id, \"Caretaker_ID FROM Aniamls WHERE Animal_ID = $1";
-    dal.query(sql, [id], (err, result) => {
-      if (err) {
-        // logging should go here
-        if(DEBUG) console.log(err);
-        reject(err);
-      } else {
-        resolve(result.rows);
-      }
-    }); 
-  }); 
-};
 
+//Edit an animal
 var patchAnimals = function(id, name, age) {
   if(DEBUG) console.log("animals.pg.dal.patchAnimal()");
   return new Promise(function(resolve, reject) {
-    const sql = "UPDATE public.\"Animals\" SET \"Name\"=$2, \"Age\"=$3,  WHERE \"Animal_ID\"=$1;";
+    //sql stattement to update the animal's data
+    const sql = "UPDATE public.\"Animals\" SET \"Name\"=$2, \"Age\"=$3 WHERE \"Animal_ID\"=$1;";
+    //query the data using the sql and data imported from the form
     dal.query(sql, [id, name, age], (err, result) => {
       if (err) {
           reject(err);
@@ -64,10 +58,13 @@ var patchAnimals = function(id, name, age) {
   });
 };
 
+//Replace animal
 var putAnimals = function(id, name, age, specie) {
   if(DEBUG) console.log("animals.pg.dal.putAnimal()");
   return new Promise(function(resolve, reject) {
+    //sql stattement to replace the animal's data
     const sql = "UPDATE public.\"Animals\" SET \"Name\"=$2, \"Age\"=$3, \"Species_Name\"=$4 WHERE \"Animal_ID\"=$1;";
+     //query the data using the sql and data imported from the form
     dal.query(sql, [id, name, age, specie], (err, result) => {
       if (err) {
           reject(err);
@@ -78,10 +75,13 @@ var putAnimals = function(id, name, age, specie) {
   });
 };
 
+//Delete animals
 var deleteAnimals = function(id) {
   if(DEBUG) console.log("animals.pg.dal.deleteSpecies()");
   return new Promise(function(resolve, reject) {
+    //sql stattement to delete the animal's data
     const sql = "DELETE FROM public.\"Animals\" WHERE \"Animal_ID\" = $1;";
+     //query the data using the sql and the ID from the URL
     dal.query(sql, [id], (err, result) => {
       if (err) {
           reject(err);
@@ -93,10 +93,10 @@ var deleteAnimals = function(id) {
 };
 
 
+//Export the methods
 module.exports = {
     getAnimals,
     addAnimal,
-    getAnimalById,
     patchAnimals,
     putAnimals,
     deleteAnimals
--- Services/pg.species.dal.js
+++ Services/pg.species.dal.js
@@ -1,14 +1,15 @@
 const dal = require("./zoo_db");
 const DEBUG = true;
-//get all actors.
+//get all species
 var getSpecies = function() {
   if(DEBUG) console.log("species.pg.dal.getSpecies()");
   return new Promise(function(resolve, reject) {
+    //The sql statement to get all the speciess
     const sql = "SELECT * FROM public.\"Species\"\
 ORDER BY \"Species_Name\" ASC;"
+//query the data using  sql
     dal.query(sql, [], (err, result) => {
       if (err) {
-        // logging should go here
         if(DEBUG) console.log(err);
         reject(err);
       } else {
@@ -17,12 +18,14 @@ ORDER BY \"Species_Name\" ASC;"
     }); 
   }); 
 };
-
+//Add species
 var addSpecies = function(spName, scName, caretaker) {
     if(DEBUG) console.log("species.pg.dal.addSpecies()");
     return new Promise(function(resolve, reject) {
+       //The sql statement to insert a new species
       const sql = "INSERT INTO public.\"Species\"(\"Species_Name\", \"Scientific_Name\",\"Caretaker_ID\") \
           VALUES ($1, $2, $3)";
+          //query the data using  sql and data from the form
       dal.query(sql, [spName, scName, caretaker], (err, result) => {
         if (err) {
             if(DEBUG) console.log(err);
@@ -34,28 +37,16 @@ var addSpecies = function(spName, scName, caretaker) {
     });
   };
 
-  var getSpecieBySpecieName = function(id) {
-    if(DEBUG) console.log("species.pg.dal.getgetSpecieBySpecieName()");
-    return new Promise(function(resolve, reject) {
-      const sql = "SELECT \"Species_Name\" AS _id, \"Caretaker_ID FROM \" Species WHERE Species_Name = $1";
-      dal.query(sql, [id], (err, result) => {
-        if (err) {
-          // logging should go here
-          if(DEBUG) console.log(err);
-          reject(err);
-        } else {
-          resolve(result.rows);
-        }
-      }); 
-    }); 
-  };
-
+  //Edit a species
   var patchSpecie = function(id, caretakerID) {
     if(DEBUG) console.log("catetakers.pg.dal.patchCaretaker()");
     return new Promise(function(resolve, reject) {
+      //The sql statement to update a  species
       const sql = "UPDATE public.\"Species\" SET \"Caretaker_ID\"=$2 WHERE \"Species_Name\"=$1;";
+      //query the data using  sql and data from the form
       dal.query(sql, [id, caretakerID], (err, result) => {
         if (err) {
+          console.log(err);
             reject(err);
           } else {
             resolve(result.rows);
@@ -64,13 +55,17 @@ var addSpecies = function(spName, scName, caretaker) {
     });
   };
 
+  //Replace a species
   var putSpecies = function(id, scName, caretakerID, ) {
     if(DEBUG) console.log("catetakers.pg.dal.patchCaretaker()");
     return new Promise(function(resolve, reject) {
+      //The sql statement to update a species
       const sql = "UPDATE public.\"Species\" SET \"Scientific_Name\"=$2, \"Caretaker_ID\" =$3 WHERE \"Species_Name\"=$1;";
+      //query the data using  sql and data from the form
       dal.query(sql, [id, scName, caretakerID], (err, result) => {
         if (err) {
             console.log(err);
+            reject(err);
           } else {
             resolve(result.rows);
           }
@@ -78,10 +73,13 @@ var addSpecies = function(spName, scName, caretaker) {
     });
   };
 
+  //Delete a species
   var deleteSpecies = function(id) {
     if(DEBUG) console.log("species.pg.dal.deleteSpecies()");
     return new Promise(function(resolve, reject) {
+      //The sql statement to delete a species
       const sql = "DELETE FROM public.\"Species\" WHERE \"Species_Name\" = $1;";
+      //query the data using  sql and the ID from the URL
       dal.query(sql, [id], (err, result) => {
         if (err) {
             reject(err);
@@ -95,7 +93,6 @@ var addSpecies = function(spName, scName, caretaker) {
 module.exports = {
     getSpecies,
     addSpecies,
-    getSpecieBySpecieName,
     patchSpecie,
     putSpecies,
     deleteSpecies
